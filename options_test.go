e98768dc59b2d45dce667fc57c31b0330f819a14
yeqown <yeqown@gmail.com>
2021-07-06 09:34:51 +0800

    tests: add test to WithErrorTagHook

--- options_test.go
+++ options_test.go
@@ -1,6 +1,7 @@
 package gormopentracing
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/opentracing/opentracing-go"
@@ -31,4 +32,19 @@ func Test_Option_WithSqlParameters(t *testing.T) {
 	opt := WithSqlParameters(false)
 	p := New(opt)
 	assert.Equal(t, false, p.(opentracingPlugin).opt.logSqlParameters)
+}
+
+func Test_WithErrorTagHook(t *testing.T) {
+	opt := defaultOption()
+	assert.NotNil(t, opt.errorTagHook)
+
+	// apply
+	var hook errorTagHook = func(sp opentracing.Span, err error) {
+		println("i do nothing with error")
+	}
+	WithErrorTagHook(hook)(opt)
+
+	v1 := reflect.ValueOf(hook)
+	v2 := reflect.ValueOf(opt.errorTagHook)
+	assert.Equal(t, v1.Pointer(), v2.Pointer())
 }
